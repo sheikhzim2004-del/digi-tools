b04493b6a0fae735d11203f6e62cf6308d0233b6
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-08-17 00:16:30 +0600

    pricing section title and subtitle addede

--- src/components/pricingSection/Pricing.jsx
+++ src/components/pricingSection/Pricing.jsx
@@ -12,7 +12,13 @@ const Pricing = () => {
 
   return (
     <section className="my-container my-[clamp(7rem,6vw,7.5rem)]">
-      
+      {/* heading */}
+      <h2 className="text-[clamp(2.75rem,5vw,3rem)] font-bold mb-2 text-center">
+        Simple, Transparent Pricing
+      </h2>
+      <p className="text-sm text-gray-400 mb-8 text-center">
+        Choose the plan that fits your needs. Upgrade or downgrade anytime.
+      </p>
 
       {/* card */}
       <div className="grid  md:grid-cols-3 gap-10 md:gap-6 lg:gap-10">
